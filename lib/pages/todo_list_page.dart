5c3c9a86f088645951e9d09b2f9ab6dd0cd92974
Gilberto Junior <gagjunior@hotmail.com>
2022-02-10 00:17:49 -0300

    Separar arquivo todo_list_page

---
+++ lib/pages/todo_list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TodoListPage extends StatelessWidget {
+  const TodoListPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromARGB(255, 221, 209, 33),
+      
+    );
+  }
+}
